658f38a795e9dc3a0023a5abbe833133e28408cc
bhaibavarora <bhaibavarora11712@gmail.com>
2026-06-28 20:12:25 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return (
+    <h1>Anime Clash PvP Simulator</h1>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
