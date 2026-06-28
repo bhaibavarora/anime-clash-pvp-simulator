650f408dbaacede073fec0b151859ca93e9413b7
bhaibavarora <bhaibavarora11712@gmail.com>
2026-06-28 22:18:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,49 @@
-import React, {useState} from "react";
-import {createRoot} from "react-dom/client";
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
 
-function App(){
+function App() {
 
-const [fighter,setFighter] = useState("");
-const [message,setMessage] = useState("");
+  const [fighter, setFighter] = useState("");
+  const [message, setMessage] = useState("");
 
-function attack(){
- setMessage(fighter + " attacks the enemy! ⚔️");
+  function attack() {
+    setMessage(fighter + " attacks the enemy ⚔️");
+  }
+
+  return (
+    <div>
+
+      <h1>⚔️ Anime Clash PvP Simulator ⚔️</h1>
+
+      <h2>Choose your fighter</h2>
+
+      <button onClick={() => setFighter("Naruto 🔥")}>
+        Naruto
+      </button>
+
+      <button onClick={() => setFighter("Goku ⚡")}>
+        Goku
+      </button>
+
+      <button onClick={() => setFighter("Ichigo 🗡️")}>
+        Ichigo
+      </button>
+
+
+      <h2>Battle Arena</h2>
+
+      <h3>{fighter}</h3>
+
+      <button onClick={attack}>
+        Attack ⚔️
+      </button>
+
+      <p>{message}</p>
+
+    </div>
+  );
 }
 
-return(
-<div>
+createRoot(document.getElementById("root")).render(<App />);
 
-<h1>⚔️ Anime Clash PvP Simulator ⚔️</h1>
 
-<h2>Choose your fighter</h2>
-
-<button onClick={()=>setFighter("Naruto 🔥")}>
-Naruto
-</button>
-
-<button onClick={()=>setFighter("Goku ⚡")}>
-Goku
-</button>
-
-<button onClick={()=>setFighter("Ichigo 🗡️")}>
-Ichigo
-</button>
-
-<h2>Battle Arena</h2>
-
-<h3>{fighter}</h3>
-
-<button onClick={attack}>
-Attack 💥
-</button>
-
-<p>{message}</p>
-
-</div>
-)  
-
-}
-
-createRoot(document.getElementById("root")).render(<App/>);
