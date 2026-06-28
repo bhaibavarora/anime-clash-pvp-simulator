279f9f08d1c4d0c560bcbee64469783a9be4c115
bhaibavarora <bhaibavarora11712@gmail.com>
2026-06-28 22:34:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,63 +1,15 @@
-import React, { useState } from "react";
-import { createRoot } from "react-dom/client";
-
-function App(){
-
-const [fighter,setFighter] = useState("");
-const [playerHP,setPlayerHP] = useState(100);
-const [enemyHP,setEnemyHP] = useState(100);
-const [message,setMessage] = useState("");
-
 function attack(){
 
 let damage = Math.floor(Math.random()*20)+5;
+let enemyDamage = Math.floor(Math.random()*10)+1;
 
-setEnemyHP(enemyHP-damage);
+setEnemyHP(enemyHP - damage);
+setPlayerHP(playerHP - enemyDamage);
 
 setMessage(
-fighter + " attacked enemy for " + damage + " damage ⚔️"
+fighter + " dealt " + damage + " damage ⚔️ | Enemy hit you for " + enemyDamage
 );
 
 }
 
-return(
-<div>
 
-<h1>⚔️ Anime Clash PvP Simulator ⚔️</h1>
-
-<h2>Choose your fighter</h2>
-
-<button onClick={()=>setFighter("Naruto 🔥")}>
-Naruto
-</button>
-
-<button onClick={()=>setFighter("Goku ⚡")}>
-Goku
-</button>
-
-<button onClick={()=>setFighter("Ichigo 🗡️")}>
-Ichigo
-</button>
-
-
-<h2>Battle Arena</h2>
-
-<h3>{fighter}</h3>
-
-<p>Your HP: {playerHP}</p>
-
-<p>Enemy HP: {enemyHP}</p>
-
-<button onClick={attack}>
-Attack ⚔️
-</button>
-
-<p>{message}</p>
-
-
-</div>
-)
-
-}
-
-createRoot(document.getElementById("root")).render(<App />);
